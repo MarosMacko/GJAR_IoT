5288114407c1cd655d6463456141b33e1d126409
Superbee29 <samuel.novak@protonmail.com>
2018-01-03 17:02:53 +0100

    POST, schemes

--- lib/APIv0.1.py
+++ lib/APIv0.1.py
@@ -1,6 +1,33 @@
 from flask import jsonify
 
+from validator import is_valid
+
 class api():
+    schemes = { "connect": {"id": 0},
+                "data": {
+                    "id": 0,
+                    "token": "",
+                    "data": {
+                        "time": "", # [01]
+                        "temperature": 0.0, # [02]
+                        "humidity": 0.0, # [02]
+                    }
+                },
+                "error": {
+                    "id": 0,
+                    "token": "",
+                    "level": "",
+                    "error": ""
+                },
+                "alive": {
+                    "id": 0,
+                    "token": ""
+                },
+                "view": {
+                    "room": 0,
+                    "time-from": "", # [01]
+                }
+                }
     def __init__(self,):
         pass
     def call(self, request):
---
+++ lib/validator.py
@@ -0,0 +1,28 @@
+def is_valid(scheme, data, strict=False):
+    """Validate data structure in compliance with the scheme."""
+    if type(scheme) == dict:
+        for s in scheme:
+            if s in data:
+                if type(scheme[s]) == type(data[s]):
+                    if type(scheme[s]) in (dict, list, tuple) and not is_valid(scheme[s], data[s], strict):
+                        return False
+                else:
+                    return False
+            elif strict:
+                return False
+        sch = set(scheme) # !!!
+        dat = set(data)
+        if not sch.intersection(dat).issubset(sch):
+            return False
+    elif type(scheme) in (list, tuple):
+        # scheme must contain 1 element
+        t = type(scheme[0])
+        if t in (dict, list, tuple):
+            for d in data:
+                if t != type(d) or not is_valid(t, d, strict):
+                    return False
+        else:
+            for d in data:
+                if t != type(d):
+                    return False
+    return True
