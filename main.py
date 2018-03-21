3943948115f570da2ea7807e5f585c17240532a4
root <root@gjar-iot.gjar-po.sk>
2018-03-21 19:53:25 +0100

    Routing + api-call help

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from flask import Flask, request, abort, send_from_directory
 from lib import api_manager
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='/var/www/static')
 
 @app.route("/")
 def index():
     with open("/var/www/static/index.html", "rb") as f:
         return f.read()
-
+"""
 @app.route("/static/<path:path>")
-def static(path):
+def static_content(path):
     return send_from_directory("/var/www/static/", path)
+"""
 
 @app.route("/api/v<version>/<req>", methods=["POST"])
 def api_call(version, req):
