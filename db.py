6dbd75b7d4b466504d0bd284023f0ae627e2d54a
Samuel Novák <samuel.novak@protonmail.com>
2018-04-10 14:39:47 +0200

    Sync

--- db.py
+++ db.py
@@ -1,13 +1,13 @@
 from flask.ext.mysql import MySQL
 
-from flask import g
+from flask import g, current_app
 
 def get_db():
     if not hasattr(g, 'db_con'):
         g.db_con = g.mysql.connect()
     return g.db_con
 
-@app.teardown_appcontext
+@current_app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'db_con'):
