8a7997b4a94b35ae6102cbbdf07c5be7e8f5d97f
Superbee29 <samuel.novak@protonmail.com>
2018-07-15 18:18:30 +0200

    Finished the automatic changelog system

---
+++ Changelogs/README.md
@@ -0,0 +1,7 @@
+*Gymnázium Jána Adama Raymana, Prešov*
+
+# GJAR IoT Project
+## Automatic Changelog
+
+1. Create a copy of `template.yml` and name it with you name, project name or both (e.g. `samo.yml`, `logging.yml`, `samo-logging.yml`).
+2. Commit this file with your changes.
--- Changelogs/changelog.py
+++ Changelogs/changelog.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import yaml
 from collections import namedtuple
+from yattag import Doc, indent
 
 parser = argparse.ArgumentParser(description="A tool for consolidating YAML changelogs.")
 parser.add_argument("-l", "--location", action="store", help="Only specify if changelog location differs from the script's location.")
@@ -15,52 +16,124 @@ def verbose(msg):
 
 # not verbose => replace the def
 if not args.verbose:
-    def verbose(msg):
+    def _verbose(msg):
         pass
-    verbose = verbose
+    verbose = _verbose
 
-files = []
-logs = []
+
+date = time.strftime("%Y-%m-%d")
 
 change = namedtuple("change", ("type", "value"))
 log = namedtuple("log", ("author", "changes"))
-counter = 0
 
-# TODO: Possibly inside a class
+class Loader():
+    allowed_types = {"add", "remove", "tweak", "bugfix"}
+    def __init__(self, loc):
+        self.counter = 0
+        self.logs = {}
 
-def load_log(s):
-    y = yaml.load(s)
-    global counter
-    counter += 1
+    def load_old(self, data):
+        self.logs = yaml.load(data)
+
+    def load_log(self, data):
+        y = yaml.load(data)
+        author = y["author"]
+        changes = []
+        for entry in y["changes"]:
+            for ch in [change(type=k, value=v) for k, v in entry.items()]:
+                if ch.type in self.allowed_types:
+                    changes.append(ch)
+                else:
+                    raise Exception("Invalid change type.")
+        if date not in self.logs:
+            self.logs[date] = []
+        self.logs[date].append(log(author=author, changes=tuple(changes)))
+        self.counter += 1
+
+    def save_yaml(self, path):
+        data = "# Automatically generated changelog archive. Not to be modified manually.\n"
+        data += yaml.dump(self.logs)
+        if not args.no_changes:
+            with open(os.path.join(path, "changelog.yml"), "w") as f:
+                f.write(data)
+            verbose("Saving changelog.yml")
+        else:
+            verbose("Would save changelog.yml")
+
+    def save(self, path):
+        verbose("Adding {} new changelogs.".format(self.counter))
+        self.save_yaml(path)
+        doc, tag, text = Doc().tagtext()
+        doc.asis('<!DOCTYPE html>')
+        doc.asis("<!-- AUTOMATICALLY GENERATED - NOT TO BE MODIFIED MANUALLY -->")
+        with tag('html'):
+            with tag("head"):
+                doc.stag("meta", charset="utf-8")
+                doc.line("title", "GJAR IoT Changelog")
+                with open(os.path.join(path, "changelog.css"), "r") as f:
+                    doc.line("style", f.read())
+            with tag('body'):
+                doc.line("h1", "GJAR IoT Changelog")
+                doc.line("i", "Last generated: {}".format(date))
+                for d in sorted(self.logs.keys()):
+                    with tag("div"):
+                        doc.line("h2", d)
+                        with tag("ul"):
+                            for item in self.logs[d]:
+                                with tag("li"):
+                                    doc.line("h3", item.author)
+                                    with tag("ul"):
+                                        for ch in item.changes:
+                                            doc.line("li", ch.value, klass="type-" + ch.type)
+
+        if not args.no_changes:
+            with open(os.path.join(path, "changelog.html"), "w") as f:
+                f.write(indent(doc.getvalue()))
+            verbose("Saving changelog.html")
+        else:
+            verbose("Would save changelog.html")
+
+
 
 if args.location:
-    dir = args.location
+    loc = args.location
 else:
-    dir = os.path.dirname(os.path.realpath(__file__))
-print("Running in directory {}...".format(dir))
-for f in os.listdir(dir):
+    loc = os.path.dirname(os.path.realpath(__file__))
+
+files = []
+loader = Loader(loc)
+print("Running in directory {}...".format(loc))
+
+if os.path.isfile(os.path.join(loc, "changelog.yml")):
+    verbose("changelog.yml file exists, loading old changelogs.")
+    with open(os.path.join(loc, "changelog.yml"), "r") as f:
+        loader.load_old(f.read())
+
+for f in os.listdir(loc):
     if not f.endswith(".yml") or f in ("template.yml", "changelog.yml"):
         verbose("Skipping file {}.".format(f))
         continue
     try:
-        with open(os.path.join(dir, f), "r") as lf:
+        with open(os.path.join(loc, f), "r") as lf:
             verbose("Reading file {}.".format(f))
             try:
-                load_log(lf.read())
+                loader.load_log(lf.read())
             except Exception:
-                print("Error when reading file {}. Skipping.".format(f))
-                print(sys.exc_info())
+                print("Error when reading file {}. Skipping.".format(f), end="\t")
+                print(sys.exc_info()[1])
                 continue
     except Exception:
         print("Error when opening file {}. Skipping.".format(f))
         continue
     files.append(f)
 
+loader.save(loc)
+
 if not args.no_changes:
-    print("Purging directory {}...".format(dir))
+    print("Purging directory {}...".format(loc))
     for f in files:
         try:
-            os.remove(os.path.join(dir, f))
+            os.remove(os.path.join(loc, f))
             verbose("Removing file {}.".format(f))
         except:
             print("Could not remove file {}.".format(f))
