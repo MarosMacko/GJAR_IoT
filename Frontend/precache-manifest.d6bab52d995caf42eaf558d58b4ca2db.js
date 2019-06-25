23f2b4413369bbc121f89c0d4d94a7874fa3efb6
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2019-06-25 19:23:03 +0200

    Added new website

---
+++ Frontend/precache-manifest.d6bab52d995caf42eaf558d58b4ca2db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc41c02ba8f262d8b075e6aaa1dfcbae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a6ebf09c9eb30b9b2d6",
+    "url": "./static/css/main.4b597ee4.chunk.css"
+  },
+  {
+    "revision": "1e14b1cc0a1863fd7237",
+    "url": "./static/js/2.f256a3c6.chunk.js"
+  },
+  {
+    "revision": "7a6ebf09c9eb30b9b2d6",
+    "url": "./static/js/main.7ea6f829.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1e9aebf7933f08cb7a44fa5a502ddf83",
+    "url": "./static/media/bluechart.1e9aebf7.svg"
+  },
+  {
+    "revision": "dcd133bc429885270afd5158926c12f3",
+    "url": "./static/media/greenchart.dcd133bc.svg"
+  },
+  {
+    "revision": "cce984aec7b8ddcfbb0e6e0f4e06fec4",
+    "url": "./static/media/purplechart.cce984ae.svg"
+  },
+  {
+    "revision": "4187a81e2a63d4b469e300caae304c64",
+    "url": "./static/media/wemos.4187a81e.jpg"
+  }
+]);
