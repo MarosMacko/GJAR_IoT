0a2d6679f9cd0fc21e0c0342f3a49354588f5d1f
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2019-10-25 19:33:59 +0200

    added build folder

---
+++ Frontend/build/precache-manifest.26c0f6bc3be83d15f8759b5c631f0bcd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b2791dd873602c2de4e16af749a442",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ff79d7954ec55ba5796",
+    "url": "./static/css/main.6765746e.chunk.css"
+  },
+  {
+    "revision": "1da16ee10ac4026d4414",
+    "url": "./static/js/2.4d296ab7.chunk.js"
+  },
+  {
+    "revision": "3ff79d7954ec55ba5796",
+    "url": "./static/js/main.92f4f4db.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1e9aebf7933f08cb7a44fa5a502ddf83",
+    "url": "./static/media/bluechart.1e9aebf7.svg"
+  },
+  {
+    "revision": "dcd133bc429885270afd5158926c12f3",
+    "url": "./static/media/greenchart.dcd133bc.svg"
+  },
+  {
+    "revision": "cce984aec7b8ddcfbb0e6e0f4e06fec4",
+    "url": "./static/media/purplechart.cce984ae.svg"
+  },
+  {
+    "revision": "4187a81e2a63d4b469e300caae304c64",
+    "url": "./static/media/wemos.4187a81e.jpg"
+  }
+]);
