9ceb3913a69707287c3ca631bdf7f55286420a10
Samuel Novák <samuel.novak@protonmail.com>
2020-01-06 15:57:21 +0100

    Validator tweak + new validator continued

--- Backend/lib/validator2.py
+++ Backend/lib/validator2.py
@@ -1,29 +1,80 @@
-from typing import Union
-from collections.abc import Iterable
+from collections.abc import Iterable, Mapping
 
-class Item():
+__all__ = ("Item", "List", "Map")
+
+def can_be_instance(item, dtype):
+    if isinstance(item, dtype):
+        return True
+    else:
+        # try:
+        #     dtype(str(item))
+        #     return True
+        # except Exception as e:
+        #     print(e)
+        #     return False
+        if dtype == float and isinstance(item, int):
+            return True
+    return False
+        
+
+class Item:
     __slots__ = ("name", "dtype")
     def __init__(self, name:str, dtype:type):
         self.name = name
         self.dtype = dtype
+
     def __repr__(self):
-        return "{}(name={}, dtype={})".format(type(self).__name__, repr(self.name), self.dtype.__name__)
-    def validate(self, value):
-        if type(self.dtype) == Union:
-            return isinstance(value, self.dtype.__args__)
+        return "{}(name={}, dtype={})".format(type(self).__name__, repr(self.name), repr(self.dtype.__name__))
+
+    def validate(self, data):
+        return can_be_instance(data, self.dtype)
+
+
+class List(Item):
+    def validate(self, data):
+        if not isinstance(data, Iterable):
+            return False
+
+        if isinstance(self.dtype, Item):
+            return all(map(lambda x: self.dtype.validate(x), data))
         else:
-            return isinstance(value, self.dtype)
+            return all(map(lambda x: can_be_instance(x, self.dtype), data))
 
 
-class Tree(Item):
+class Map(Item):
     __slots__ = ("children",)
-    def __init__(self, name:str, children:list):
-        super().__init__(name, type(self))
+    def __init__(self, name:str, children:Iterable, optional:Iterable=None):
+        self.name = name
+        self.dtype = type(self)
         self.children = children
+        self.optional = optional
+
     def __repr__(self):
         return super().__repr__() \
-            + "\n" + "\n".join(map(lambda x: "\n".join(["  " + l for l in x.splitlines()]),
+            + "\n" + "\n".join(map(lambda x: "\n".join(["|- " + l for l in x.splitlines()]),
                                    [repr(c) for c in self.children]))
 
-    def validate(self, value):
-        return isintance(value, Iterable) # and all(###)
+    def validate(self, data):
+        if not isinstance(data, Mapping):
+            return False
+
+        keys = list(data.keys())
+        
+        for item in self.children:
+            try:
+                d = data[item.name]
+            except:
+                if item.name in self.optional:
+                    continue
+                else:
+                    return False
+            
+            if item.validate(d):
+                keys.remove(item.name)
+            else:
+                return False
+
+        if keys:
+            return False
+        return True
+
