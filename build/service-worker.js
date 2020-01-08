e85255c84ed6c73bb9456a19d35b1a24d099ada0
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2020-01-08 22:10:42 +0100

    added graph settings

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2a9509af4ad7c7855a9e9e6ae59322e4.js"
+  "./precache-manifest.0b52a80a88c535f0eb9ca8e9e1e651c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
